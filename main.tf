3e401a388a477b98d8cddfe2623d80c7496b3e95
Rafael de Christo Marques <rafael.marques@a3data.com.br>
2022-10-19 16:59:01 -0300

    feat: add main terraform

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("usuario.json")
+  project = "axiomatic-treat-361001"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+# Cloud Storage
+
+resource "google_storage_bucket" "default" {
+  name = "bucket-atlas-violencia"
+  location = "US"
+}
