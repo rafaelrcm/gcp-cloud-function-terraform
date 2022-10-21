649c635637bee9f71a609bec1b16ea46b7b0f67a
Rafael de Christo Marques <rafael.marques@a3data.com.br>
2022-10-20 21:55:49 -0300

    feat: adicionando arquivo schema

--- bigquery.tf
+++ bigquery.tf
@@ -8,32 +8,5 @@ resource "google_bigquery_table" "default" {
   description                = "Homicídios por Estado"
   table_id                   = "homicidios_por_estado"
   depends_on                 = [google_bigquery_dataset.default]
-  schema                     = <<EOF
-  [
-    {
-      "name": "codigo",
-      "type": "INTEGER",
-      "mode": "NULLABLE",
-      "description": "Codigo do Estado"
-    },
-    {
-      "name": "estado",
-      "type": "STRING",
-      "mode": "NULLABLE",
-      "description": "Nome do Estado"
-    },
-    {
-      "name": "ano",
-      "type": "INTEGER",
-      "mode": "NULLABLE",
-      "description": "Ano da Pesquisa"
-    },
-    {
-      "name": "mortes",
-      "type": "INTEGER",
-      "mode": "NULLABLE",
-      "description": "Quantidade de Mortes"
-    }
-  ]
-  EOF
+  schema                     = "schema.json"
 }
